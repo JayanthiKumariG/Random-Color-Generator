14f088a6b4f86632b83f8498e83d58c7ceab3722
SAI KIRAN GUNDEMONI <gundemonijayanthi@gmail.com>
2024-09-09 19:32:36 +0530

    basic random color generator created

---
+++ app.js
@@ -0,0 +1,12 @@
+const getColor=()=>{
+    const randomNumber=Math.floor(Math.random()*16777215);
+    const randomCode="#" + randomNumber.toString(16)
+    //console.log(randomNumber,randomcode)
+    document.body.style.backgroundColor=randomCode;
+    document.getElementById("code").innerText=randomCode;
+
+}
+document.getElementById("btn").addEventListener(
+    "click",getColor
+)
+getColor();
